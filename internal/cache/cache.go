a3fe344615cc6705d76fe90b9b1baf0552ce0750
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-24 18:30:33 +0330

    cache package implemented

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -18,3 +18,14 @@ type Cache struct {
 func (c *Cache) Init() {
 	c.list = make(map[int]Item)
 }
+
+func (c *Cache) Push(id int, start time.Time) {
+	c.list[id] = Item{
+		Start:  start,
+		Status: false,
+	}
+}
+
+func (c *Cache) Pull(id int) Item {
+	return c.list[id]
+}
